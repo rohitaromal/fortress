4147b4108da978102e93d2c823735b048b39fdb5
rohitaromal <61880215+rohitaromal@users.noreply.github.com>
2020-05-01 22:26:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+function setup() {
+  createCanvas(800,400);
+  //createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background("black");  
+
+//  fill("red");
+
+
+strokeWeight(4);
+stroke("F85B00");
+
+fill("orange");
+ rect(400,200,300,600);
+ fill("orange");
+
+rect(400,200,40,80);
+ fill("orange");
+ rect(660,200,40,80);
+fill("orange");
+rect(520,30,60,200);
+fill("red");
+ellipse(413, 200, 55, 55);
+fill("red");
+ellipse(685, 200, 55, 55);
+fill("red");
+rect(520,300,60,100);
+
+
+
+
+
+
+
+  
+  
+  
+  
+  drawSprites();
+}
